4bf2eef3059b0453eb11287672cf84ef60282e4c
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-10 03:57:07 -0300

    adding go to both f().none of them show

--- GoRoutines2/main.go
+++ GoRoutines2/main.go
@@ -7,9 +7,11 @@ import (
 func main() {
 	//here we have an anonymous function. it does no contain any name
 	//useful to create an inline function
-	func() {
+	go func() {
 		fmt.Println("hello")
 	}()
+	go func() {
+		fmt.Println("world")
+	}() //as I have both f() with go. none of them will show
 	fmt.Println("END")
-
 }
