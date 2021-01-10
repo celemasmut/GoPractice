8f22f5239eaf792e52909cbd518938493034cb91
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-10 03:59:45 -0300

    adding select{}

--- GoRoutines2/main.go
+++ GoRoutines2/main.go
@@ -14,4 +14,5 @@ func main() {
 		fmt.Println("world")
 	}() //as I have both f() with go. none of them will show
 	fmt.Println("END")
+	select {} //this will cause an indefinitly run ending with deadlock
 }
