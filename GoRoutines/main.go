f26b181bb8f20298d90dd3cef83d769fb78d1f9d
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-10 03:12:20 -0300

    Adding time

--- GoRoutines/main.go
+++ GoRoutines/main.go
@@ -15,5 +15,6 @@ func heavy() {
 func main() {
 	//go es a word to make work the concurrency
 	go heavy()
-	fmt.Println("END")
+	fmt.Println("END")          //heavy and this text will start at the same time so END will be shown first
+	time.Sleep(time.Second * 5) //as I have this time the f.heavy() works
 }
